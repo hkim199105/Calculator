7eab670c0fbf476d152c025caa531527b3cb3431
hkim <hkim199105@gmail.com>
2020-11-30 00:37:32 +0900

    4 rules calculation

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -7,6 +7,14 @@
 
 import SwiftUI
 
+enum CalculatorButtonType: String {
+    case number
+    case symbol
+    case decimal
+    case equals
+    case etc
+}
+
 enum CalculatorButton: String {
     case zero, one, two, three, four, five, six, seven, eight, nine
     case equals, plus, minus, multiply, divide
@@ -15,45 +23,112 @@ enum CalculatorButton: String {
     
     var title: String {
         switch self {
-        case .zero: return "0"
-        case .one: return "1"
-        case .two: return "2"
-        case .three: return "3"
-        case .four: return "4"
-        case .five: return "5"
-        case .six: return "6"
-        case .seven: return "7"
-        case .eight: return "8"
-        case .nine: return "9"
-        case .plus: return "+"
-        case .minus: return "-"
-        case .multiply: return "X"
-        case .divide: return "➗"
-        case .plusMinus: return "+/-"
-        case .percent: return "%"
-        case .ac: return "AC"
-        case .equals: return "="
-        case .decimal: return "."
+        case .zero:         return "0"
+        case .one:          return "1"
+        case .two:          return "2"
+        case .three:        return "3"
+        case .four:         return "4"
+        case .five:         return "5"
+        case .six:          return "6"
+        case .seven:        return "7"
+        case .eight:        return "8"
+        case .nine:         return "9"
+        case .plus:         return "+"
+        case .minus:        return "-"
+        case .multiply:     return "x"
+        case .divide:       return "÷"
+        case .equals:       return "="
+        case .decimal:      return "."
+        case .plusMinus:    return "+/-"
+        case .percent:      return "%"
+        case .ac:           return "AC"
+        }
+    }
+    
+    var type: CalculatorButtonType {
+        switch self {
+        case .zero, .one, .two, .three, .four, .five, .six, .seven, .eight, .nine:
+            return .number
+        case .plus, .minus, .divide, .multiply:
+            return .symbol
+        case .decimal:
+            return .decimal
+        case .equals:
+            return .equals
+        default:
+            return .etc
         }
     }
     
     var background: Color {
-        switch self {
-        case .zero, .one, .two, .three, .four, .five, .six, .seven, .eight, .nine:
+        switch self.type {
+        case .number:
             return Color(.darkGray)
-        case .ac, .plusMinus, .percent:
-            return Color(.lightGray)
-        default:
+        case .symbol:
             return .orange
+        case .decimal:
+            return Color(.lightGray)
+        case .equals:
+            return Color(.lightGray)
+        case .etc:
+            return Color(.lightGray)
         }
     }
 }
 
 class GlobalEnvironment: ObservableObject {
-    @Published var display = ""
+    @Published var display = "0"
+    var lastSymbol:CalculatorButton = .plus
+    var lastElement = ""
+    var mustInit = true
     
     func receiveInput(calculatorButton: CalculatorButton) {
-        self.display = calculatorButton.title
+        switch calculatorButton.type {
+        case .number:
+            if mustInit {
+                self.display = calculatorButton.title
+                mustInit = false
+            } else {
+                self.display = self.display + calculatorButton.title
+            }
+        case .decimal:
+            self.display = self.display + calculatorButton.title
+        case .symbol:
+            if !lastElement.isEmpty && !mustInit {
+                self.display = calculate(first: self.lastElement, second: self.display, symbol: self.lastSymbol)
+            }
+            self.lastElement = self.display
+            self.lastSymbol = calculatorButton
+            mustInit = true
+        case .equals:
+            if !mustInit {
+                self.display = calculate(first: self.lastElement, second: self.display, symbol: self.lastSymbol)
+            }
+            self.lastSymbol = .plus
+            self.lastElement = ""
+            mustInit = true
+        case .etc:
+            if calculatorButton.title == "AC" {
+                self.display = "0"
+                self.lastElement = ""
+                self.lastSymbol = .plus
+            } else {
+                self.display = self.display + calculatorButton.title
+            }
+        }
+    }
+    
+    func calculate(first: String, second: String, symbol: CalculatorButton) -> String {
+        let firstNum = NSString(string: first).doubleValue
+        let secondNum = NSString(string: second).doubleValue
+
+        switch symbol {
+        case .plus: return String(describing: firstNum + secondNum)
+        case .minus: return String(describing: firstNum - secondNum)
+        case .multiply: return String(describing: firstNum * secondNum)
+        case .divide: return String(describing: firstNum / secondNum)
+        default: return ""
+        }
     }
 }
 
